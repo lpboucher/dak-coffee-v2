e3b3a8ba4c3633c37605cdcea8a28ca5242d49c4
LP Boucher <lp_boucher@hotmail.com>
2019-11-08 10:29:50 +0100

    styles

--- client/src/components/utils/WithHover.js
+++ client/src/components/utils/WithHover.js
@@ -5,6 +5,7 @@ import { Box } from 'grommet';
 const WithHover = styled(Box)`
     opacity: ${props => props.isHoverable ? 1 : 0.5};
     cursor: pointer;
+    text-transform: uppercase;
     position: relative;
     &:hover {
         opacity: 0.5;
