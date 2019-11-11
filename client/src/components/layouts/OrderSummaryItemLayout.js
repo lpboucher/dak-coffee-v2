19e9f3c1809870dbce4ab3b86fa8825e3ca1516c
LP Boucher <lp_boucher@hotmail.com>
2019-11-11 12:59:58 +0100

    only show subtotal in cart, and add link to checkout flow

--- client/src/components/layouts/OrderSummaryItemLayout.js
+++ client/src/components/layouts/OrderSummaryItemLayout.js
@@ -4,7 +4,7 @@ import { Box, Text } from 'grommet';
 
 const OrderSummaryItemLayout = ({label, price}) => {
     return (
-        <Box width="100%" direction="row" justify="end" >
+        <Box width="100%" pad={{top: 'medium'}} direction="row" justify="end" >
             <Box width="200px"><Text textAlign="end">{label}</Text></Box>
             <Box width="100px"><Text textAlign="end">{price}</Text></Box>
         </Box>
