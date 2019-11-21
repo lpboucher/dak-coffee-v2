f884188f2e8cf2abfe240cd9e8077521c420fe9e
LP Boucher <lp_boucher@hotmail.com>
2019-11-21 20:17:30 +0100

    use grommet for overlay animation to remove react-reveal dependency

--- client/src/components/presentation/global/Hero.js
+++ client/src/components/presentation/global/Hero.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { withTranslation } from 'react-i18next';
 import withResponsive from '../../HOCs/withResponsive';
-import Slide from 'react-reveal/Slide';
 
 import { Box, Image, Stack, Text } from 'grommet';
 
@@ -19,13 +18,11 @@ const Hero = ({bgImage, height="90vh", overlay, t, media}) => {
             />
         </Box>
         {isNotSmall &&
-        <Slide right duration={1500}>
-        <Box height={overlay.height} width={overlay.width} justify={overlay.justify || "center"} background={`${overlay.withOpacity ? 'rgba(0,0,0,0.5)' : ''}`}>
+        <Box animation={"slideRight"} height={overlay.height} width={overlay.width} justify={overlay.justify || "center"} background={`${overlay.withOpacity ? 'rgba(0,0,0,0.5)' : ''}`}>
             <Box width="50vw" pad={{'horizontal': 'large'}}>
                 <Text size="large" color='white' weight="bold" >{t(overlay.text)}</Text>
             </Box>
         </Box>
-        </Slide>
         }
     </Stack>
     );
