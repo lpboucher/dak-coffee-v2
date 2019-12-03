7b90a7648b0f6d258a14df0fdb35908afec8051f
LP Boucher <lp_boucher@hotmail.com>
2019-12-03 15:27:09 +0100

    move meta tags to SEO component using react helmet

---
+++ client/src/components/utils/SEO/SEO.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Helmet from "react-helmet";
+
+const SEO = ({title, description, keywords, schema}) => {
+    return (
+        <Helmet>
+            <html lang="en"/>
+            <title>{title}</title>
+            <meta name="description" content={description}/>
+            <meta name="keywords" content={keywords.join(",")}/>
+            <meta name="og:title" content={title}/>
+            <meta name="og:description" content={description}/>
+            <meta name="twitter:title" content={title}/>
+            <meta name="twitter:description" content={description}/>
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+            <meta name="theme-color" content="#000000"/>
+            <script type="application/ld+json">{JSON.stringify(schema)}</script>
+        </Helmet>
+    );
+};
+
+export default SEO;
