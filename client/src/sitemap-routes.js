b9bc2cb8c525a1678b36fbbf0a0f8a5e8b13a0aa
LP Boucher <lp_boucher@hotmail.com>
2019-12-16 18:03:02 +0100

    remove christmas from sitemap generator

--- client/src/sitemap-routes.js
+++ client/src/sitemap-routes.js
@@ -16,6 +16,5 @@ export default (
           <Route path="/privacy" />
           <Route path="/blog/:slug" />
           <Route path="/blog" />
-          <Route path="/christmas" />
         </Switch>
 );
