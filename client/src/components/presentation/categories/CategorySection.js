d84aa8751a366782d5f2bda76b9c3ca67f33a340
LP Boucher <lp_boucher@hotmail.com>
2019-12-16 17:39:12 +0100

    sort product list by most recent

--- client/src/components/presentation/categories/CategorySection.js
+++ client/src/components/presentation/categories/CategorySection.js
@@ -9,12 +9,13 @@ import { Box, Heading } from 'grommet';
 
 const CategorySection = ({slug, products, media, t}) => {
     const layout = categorySectionLayout(media);
-    const header = slug.split("_")[0] === "related" ? t(`sections.related.title`) : t(`sections.shop.${slug}`)
+    const header = slug.split("_")[0] === "related" ? t(`sections.related.title`) : t(`sections.shop.${slug}`);
+    const byMostRecent = (a,b) => new Date(b.createdAt) - new Date(a.createdAt); 
     return (
         <Box background={"lightGrey"}>
             <Heading level={1} size={layout.size} textAlign="center" style={{paddingTop: '48px'}}>{header}</Heading>
             <Box direction="row" pad="large" wrap>
-                {products.map(product => 
+                {products.sort(byMostRecent).map(product => 
                     <Box key={product.id} background="mainWhite" height="350px" width={layout.width} pad="medium" margin={'1%'}>
                         <ProductCardContainer id={product.id}/>
                     </Box>
