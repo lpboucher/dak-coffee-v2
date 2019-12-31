f37ad6754bf34e5e59ad9593f547b89084462f89
LP Boucher <lp_boucher@hotmail.com>
2019-12-31 17:18:42 +0100

    prevent overwriting meta tags with undefined

--- client/src/components/utils/SEO/SEO.js
+++ client/src/components/utils/SEO/SEO.js
@@ -1,20 +1,23 @@
 import React from "react";
 import Helmet from "react-helmet";
 
-const SEO = ({title, description, keywords, schema}) => {
+const SEO = ({title, description, image, url, keywords, schema}) => {
     return (
         <Helmet>
             <html lang="en"/>
-            <title>{title}</title>
-            <meta name="description" content={description}/>
-            <meta name="keywords" content={keywords ? keywords.join(",") : ''}/>
-            <meta name="og:title" content={title}/>
-            <meta name="og:description" content={description}/>
-            <meta name="twitter:title" content={title}/>
-            <meta name="twitter:description" content={description}/>
+            {title && <title>{title}</title>}
+            {title && <meta name="og:title" content={title}/>}
+            {title && <meta name="twitter:title" content={title}/>}
+            {description && <meta name="description" content={description}/>}
+            {description && <meta name="og:description" content={description}/>}
+            {description && <meta name="twitter:description" content={description}/>}
+            {image && <meta property="og:image" content={image} />}
+            {image && <meta name="twitter:card" content={image}></meta>}
+            {url && <meta property="og:url" content={url} />}
+            {keywords && <meta name="keywords" content={keywords ? keywords.join(",") : ''}/>}
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
             <meta name="theme-color" content="#000000"/>
-            <script type="application/ld+json">{JSON.stringify(schema)}</script>
+            {schema && <script type="application/ld+json">{JSON.stringify(schema)}</script>}
         </Helmet>
     );
 };
