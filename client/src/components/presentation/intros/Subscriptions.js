9a2339c8ea16c5ba013b91ca47b024a5087465b5
LP Boucher <lp_boucher@hotmail.com>
2020-01-25 18:39:13 +0100

    simplify subscriptions and limited edition intros

--- client/src/components/presentation/intros/Subscriptions.js
+++ client/src/components/presentation/intros/Subscriptions.js
@@ -4,24 +4,24 @@ import TwoColLayout from '../../layouts/TwoColLayout';
 import FullImg from '../../utils/FullImg';
 import IntroLayout from '../../layouts/IntroLayout';
 
-const introImg = "https://res.cloudinary.com/dak-coffee-roasters/image/upload/f_auto,q_auto:low/v1568043804/Intros/SubscriptionMain_yhoylm.jpg";
+const introImg = "https://res.cloudinary.com/dak-coffee-roasters/image/upload/f_auto,q_auto:low/v1579957755/Intros/BoxShipping_igg0f9.png";
 
 const SubscriptionIntro = ({currency}) => {
     const prices = {EUR: '€25', CAD: '$35'}
     const intro = 
         <IntroLayout 
             heading="intros.subscription.title" 
-            helperText="intros.subscription.helper"
-            description="intros.subscription.description"
-            btnLabel="intros.subscription.button"
+            snap="intros.subscription.short description"
+            summaryText="intros.subscription.helper"
+            ctaLabel="intros.subscription.button"
             link="/subscriptions"
             options={{ price: prices[currency] }}
         />
     return (
         <TwoColLayout 
-            bgColor="lightGrey"
-            left={<FullImg imgLink={introImg}/>}
-            right={intro}
+            bgColor="darkGrey"
+            right={<FullImg imgLink={introImg} size="contain"/>}
+            left={intro}
         />
     );
 };
