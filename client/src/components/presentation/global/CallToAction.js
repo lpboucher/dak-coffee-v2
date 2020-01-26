6a31194fa973c7a9fdc8666e54cab0e8e81cc45a
LP Boucher <lp_boucher@hotmail.com>
2020-01-26 19:10:27 +0100

    responsive styles

--- client/src/components/presentation/global/CallToAction.js
+++ client/src/components/presentation/global/CallToAction.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { withTranslation } from 'react-i18next';
+import withResponsive from '../../HOCs/withResponsive';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -7,13 +8,16 @@ import { Box, Text } from 'grommet';
 
 import { ReactComponent as Arrow} from '../../../assets/icons/arrow.svg';
 
+import { callToActionLayout } from '../../layouts/globalResponsiveLayout';
+
 const LinkNoHeight = styled(Link)`
     line-height: 0;
 `
 
-const CallToAction = ({color='#444444', link, label, t}) => {
+const CallToAction = ({color='#444444', link, label, t, media}) => {
+    const layout = callToActionLayout(media);
     return (
-        <Box align="center" direction="row" margin={{top: '40px'}}>
+        <Box align="center" justify={layout.align} direction="row" margin={{top: '40px'}}>
             <Link to={link}>
                 <Text size="mid" color={color} weight="bold" margin={{right: '20px'}}>
                         {t(label)}
@@ -26,4 +30,4 @@ const CallToAction = ({color='#444444', link, label, t}) => {
     );
 };
 
-export default withTranslation()(CallToAction);
+export default withTranslation()(withResponsive(CallToAction));
