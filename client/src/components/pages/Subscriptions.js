1ad8724fedefda4ad990e3e1714b428c5c5e4271
LP Boucher <lp_boucher@hotmail.com>
2020-02-06 15:19:07 +0100

    Review if subscription page, remove description and add intro component

--- client/src/components/pages/Subscriptions.js
+++ client/src/components/pages/Subscriptions.js
@@ -1,18 +1,22 @@
 import React, { lazy, Suspense } from 'react';
+import { useSelector } from 'react-redux';
+import { getDisplayCurrency } from '../../ducks/views';
 
 import SEO from '../utils/SEO/SEO';
 import Hero from '../presentation/global/Hero';
 import Loader from '../utils/SimpleLoader';
-import SubscriptionDescription from '../presentation/subscriptions/SubscriptionDescription';
+import SubscriptionPageIntro from '../presentation/intros/SubscriptionPage';
+import SubscriptionSelectionLayout from '../layouts/SubscriptionSelectionLayout';
 
-const SubscriptionSpecs = lazy(() => import('../presentation/subscriptions/SubscriptionSpecs'));
 const SubscriptionContainer = lazy(() => import('../containers/subscriptions/SubscriptionsContainer'));
 const RelatedContainer = lazy(() => import('../containers/related/RelatedContainer'));
 const NewsletterContainer = lazy(() => import('../containers/newsletter/NewsletterContainer'));
 
-const header = 'https://res.cloudinary.com/dak-coffee-roasters/image/upload/f_auto,q_auto:low/v1568042393/Heros/SubscriptionHeader_ikwigv.jpg'
+const header = 'https://res.cloudinary.com/dak-coffee-roasters/image/upload/f_auto,q_auto:low/v1580997312/Heros/Header_Subscription_lcfz3j.jpg'
 
 const Subscription = () => {
+    const currency = useSelector(state => getDisplayCurrency(state));
+    const prices = {EUR: '€22', CAD: '$40'};
     return (
     <>
         <SEO canon="https://www.dakcoffeeroasters.com/subscriptions" />
@@ -20,13 +24,17 @@ const Subscription = () => {
             bgImage={header}
             overlay={{
                 text: "hero.subscription",
-                loc: "bottom-left",
-                height: "50vh"
+                loc: "left",
+                height: "50vh",
+                width: "40vw",
+                cta: "hero.cta.subscription",
+                options: { price: prices[currency] }
             }}
+            ctaLink='/shop'
         />
-        <SubscriptionDescription />
+        <SubscriptionPageIntro />
+        <SubscriptionSelectionLayout />
         <Suspense fallback={<Loader />}>
-            <SubscriptionSpecs />
             <SubscriptionContainer />
             <RelatedContainer relatedSlug="related_subscription"/>
             <NewsletterContainer />
---
+++ client/src/components/presentation/intros/SubscriptionPage.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import TwoColLayout from '../../layouts/TwoColLayout';
+import FullImg from '../../utils/FullImg';
+import IntroLayout from '../../layouts/IntroLayout';
+
+const introImg = "https://res.cloudinary.com/dak-coffee-roasters/image/upload/f_auto,q_auto/v1580998386/Intros/dakbags_xfwwrv.png";
+
+const SubscriptionPageIntro = ({currency='EUR'}) => {
+    const prices = {EUR: '€22', CAD: '$35'}
+    const intro = 
+        <IntroLayout 
+            heading="intros.subscriptionPage.title" 
+            snap="intros.subscriptionPage.short description"
+            alert="intros.subscriptionPage.alert"
+            summaryText="intros.subscriptionPage.helper"
+            ctaLabel="intros.subscriptionPage.button"
+            link="/subscriptions"
+            options={{ price: prices[currency] }}
+        />
+    return (
+        <TwoColLayout 
+            bgColor="mainWhite"
+            left={<FullImg imgLink={introImg} size="contain"/>}
+            right={intro}
+        />
+    );
+};
+
+export default SubscriptionPageIntro;
