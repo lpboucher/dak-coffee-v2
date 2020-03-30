2cbdae7ecebcf99675d1a63b8cbd11ff0f50d609
LP Boucher (AMS) <louis.boucher@achtzig20.de>
2020-03-30 15:54:43 +0200

    add shipping page

--- client/src/sitemap-routes.js
+++ client/src/sitemap-routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
- 
+
 export default (
     <Switch>
           <Route path="/shop/:slug" />
@@ -14,6 +14,7 @@ export default (
           <Route path="/faq" />
           <Route path="/terms" />
           <Route path="/privacy" />
+          <Route path="/shipping" />
           <Route path="/blog/:slug" />
           <Route path="/blog" />
         </Switch>
