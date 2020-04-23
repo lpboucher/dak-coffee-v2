3308fb2338cdfc5b6a8de200c6e6c4dd96cee8ad
LP Boucher (AMS) <louis.boucher@achtzig20.de>
2020-04-23 18:38:13 +0200

    change featured coffees

--- client/src/components/layouts/CoffeeRowLayout.js
+++ client/src/components/layouts/CoffeeRowLayout.js
@@ -4,6 +4,7 @@ import withResponsive from '../HOCs/withResponsive';
 import { Link } from 'react-router-dom';
 
 import CoffeeCardContainer from '../containers/products/CoffeeCardContainer';
+import ProductSlider from './new/ProductsSliderLayout';
 
 import { Box } from 'grommet';
 
@@ -11,8 +12,8 @@ import { coffeeRowLayout } from './globalResponsiveLayout';
 
 const CoffeeRow = ({coffees, media, t}) => {
     const layout = coffeeRowLayout(media);
-    const selectedCoffees = ["5e9da8381a71db29edc10ee1", "5e5fb5cf43c13f731d449d5b", "5e2f3f8f67724418d2e2df99", "5db9a56a4440f38ecc4c362b"];
-    // const selectedCoffees =  ["5db9a0a0ab6bee8d8e4e3902", "5db9a240646d6f8e7d7f3813", "5db9a46f4440f38ecc4c362a", "5db9a56a4440f38ecc4c362b"];
+    const selectedCoffees = ["5e5bc28f43c13f731d449d59", "5e5fb5cf43c13f731d449d5b", "5de625c5ed80d161c56b8516", "5db9a56a4440f38ecc4c362b"];
+    //const selectedCoffees =  ["5db9a0a0ab6bee8d8e4e3902", "5db9a240646d6f8e7d7f3813", "5db9a46f4440f38ecc4c362a", "5db9a56a4440f38ecc4c362b"];
     const filteredCoffees = coffees.filter(coffee => selectedCoffees.indexOf(coffee) !== -1);
     return (
     <Box pad={layout.pad} direction="row" justify="between" background="mainWhite" wrap>
@@ -22,6 +23,16 @@ const CoffeeRow = ({coffees, media, t}) => {
         </Box>
         ))}
     </Box>
+    /*<ProductSlider
+      slides={
+        [...coffees, ...coffees].map(id => (
+          <Box key={id} width={layout.itemWidth}>
+              <CoffeeCardContainer id={id}/>
+          </Box>
+          ))
+      }
+    >
+  </ProductSlider>*/
     );
 };
 
