8acbb41a4e74b86d0748fc842efbb12d842495cc
LP Boucher (AMS) <louis.boucher@achtzig20.de>
2020-04-23 10:44:23 +0200

    select coffees to display on home page

--- client/src/components/layouts/CoffeeRowLayout.js
+++ client/src/components/layouts/CoffeeRowLayout.js
@@ -11,14 +11,17 @@ import { coffeeRowLayout } from './globalResponsiveLayout';
 
 const CoffeeRow = ({coffees, media, t}) => {
     const layout = coffeeRowLayout(media);
+    const selectedCoffees = ["5e9da8381a71db29edc10ee1", "5e5fb5cf43c13f731d449d5b", "5e2f3f8f67724418d2e2df99", "5db9a56a4440f38ecc4c362b"];
+    // const selectedCoffees =  ["5db9a0a0ab6bee8d8e4e3902", "5db9a240646d6f8e7d7f3813", "5db9a46f4440f38ecc4c362a", "5db9a56a4440f38ecc4c362b"];
+    const filteredCoffees = coffees.filter(coffee => selectedCoffees.indexOf(coffee) !== -1);
     return (
-    <Box pad={layout.pad} direction="row" justify="between" background="mainWhite" wrap> 
-        {coffees.slice(0,4).map(id => (
+    <Box pad={layout.pad} direction="row" justify="between" background="mainWhite" wrap>
+        {filteredCoffees.map(id => (
         <Box key={id} width={layout.itemWidth}>
             <CoffeeCardContainer id={id}/>
         </Box>
         ))}
-    </Box> 
+    </Box>
     );
 };
 
