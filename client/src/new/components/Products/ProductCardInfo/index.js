7646d56c9f9cf8e53d4f3ea42bb113771fc202c5
LP Boucher (AMS) <louis.boucher@achtzig20.de>
2020-05-20 00:22:19 +0200

    update product card with add to cart

--- client/src/new/components/Products/ProductCardInfo/index.js
+++ client/src/new/components/Products/ProductCardInfo/index.js
@@ -1,28 +1,44 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { Text } from 'grommet';
-
+import { Box, Text } from 'grommet';
 import Skeleton from 'react-loading-skeleton';
+
+import AddToCart from '../../../containers/Cart/AddToCart';
 
 const TruncateText = styled(Text)`
     text-overflow: ellipsis;
     white-space: nowrap;
     overflow: hidden;
+    text-transform: uppercase;
+    flex-grow:1;
 `
 
-const ProductCardInfo = ({displayedPrice, title, subTitle, helper}) => {
+const FixedText = styled(Text)`
+  width: 100px;
+`
+
+const ProductCardInfo = ({id, displayedPrice, title, subTitle, helper}) => {
     return (
         <>
-          <TruncateText textAlign="start" size="medium" weight="bold" style={{textTransform: 'uppercase'}}>
-            {title || <Skeleton />}
-          </TruncateText>
-          <TruncateText textAlign="start" size="small" color="grey">
-            {subTitle || <Skeleton />}
-          </TruncateText>
-          <Text textAlign="start" weight="bold" size="xsmall" color="mainDark">
-            {displayedPrice || <Skeleton />}
-          </Text>
+          <Box direction="row" justify="between">
+            <TruncateText textAlign="start" size="medium" weight="bold" >
+              {title || <Skeleton />}
+            </TruncateText>
+            <FixedText textAlign="end">{displayedPrice}</FixedText>
+          </Box>
+          <Box direction="row" align="center" justify="between">
+            <Box>
+              <TruncateText textAlign="start" size="small" color="grey">
+                {subTitle || <Skeleton />}
+              </TruncateText>
+              <Text textAlign="start" weight="bold" size="xsmall" color="mainDark">
+                {displayedPrice || <Skeleton />}
+              </Text>
+            </Box>
+            <AddToCart productId={id} />
+          </Box>
+
         </>
     );
 };
