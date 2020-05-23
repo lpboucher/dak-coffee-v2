35bc9dba65ea751e1f6c3cfbfc22576e4b3d34ee
LP Boucher (AMS) <louis.boucher@achtzig20.de>
2020-05-24 00:37:03 +0200

    fixed action creators based on new api strcuture

--- client/src/ducks/products.js
+++ client/src/ducks/products.js
@@ -7,24 +7,30 @@ import { getCollectionBySlug } from './collections';
 export const FETCH_PRODUCTS_REQUEST = 'products/fetch_products_request';
 export const FETCH_PRODUCTS_SUCCESS = 'products/fetch_products_success';
 export const FETCH_PRODUCTS_FAILURE = 'products/fetch_products_failure';
+export const FETCH_PRODUCT_SUCCESS = 'products/fetch_product_success';
 
 //Action Creators
-export const fetchProducts = () => async dispatch => {
-    dispatch({ type: FETCH_PRODUCTS_REQUEST });
-    try {
-        const res = await axios.get(`${process.env.REACT_APP_API_PREFIX}/productsinventory`);
-        dispatch({ type: FETCH_PRODUCTS_SUCCESS, payload: res.data.products, count: res.data.count });
-    } catch(err) {
-        dispatch({ type: FETCH_PRODUCTS_FAILURE, payload: {global: "error.products.fetch"}});
-    }
-};
-
-// new
-export const fetchAllProducts = () => async dispatch => {
+export const fetchProducts = (query = []) => async dispatch => {
+  let path = `/products`;
+  if (query.length > 0) {
+    path += `?${query.join("&")}`
+  }
   dispatch({ type: FETCH_PRODUCTS_REQUEST });
   try {
-      const res = await axios.get(`${process.env.REACT_APP_API_PREFIX}/productsinventory`);
+      const res = await axios.get(`${process.env.REACT_APP_API_PREFIX}${path}`);
       dispatch({ type: FETCH_PRODUCTS_SUCCESS, payload: res.data.products, count: res.data.count });
+  } catch(err) {
+      dispatch({ type: FETCH_PRODUCTS_FAILURE, payload: {global: "error.products.fetch"}});
+  }
+};
+
+export const fetchOneProduct = (id) => async dispatch => {
+  const path = `/products/${id}`;
+  dispatch({ type: FETCH_PRODUCTS_REQUEST });
+  try {
+      const res = await axios.get(`${process.env.REACT_APP_API_PREFIX}${path}`);
+      console.log(res);
+      dispatch({ type: FETCH_PRODUCT_SUCCESS, payload: res.data.product, count: res.data.count });
   } catch(err) {
       dispatch({ type: FETCH_PRODUCTS_FAILURE, payload: {global: "error.products.fetch"}});
   }
@@ -41,6 +47,11 @@ const byId = (state = {}, action) => {
                 return obj
             }, {}),
         }
+        case FETCH_PRODUCT_SUCCESS:
+        return {
+            ...state,
+            [action.payload.id]: { ...action.payload }
+        }
         default:
             return state
     }
@@ -50,6 +61,8 @@ const allIds = (state = [], action) => {
     switch (action.type) {
         case FETCH_PRODUCTS_SUCCESS:
         return action.payload.map(product => product.id)
+        case FETCH_PRODUCT_SUCCESS:
+        return [ ...state, action.payload.id ]
         default:
         return state
     }
@@ -58,6 +71,7 @@ const allIds = (state = [], action) => {
 const total = (state = 0, action) => {
   switch (action.type) {
       case FETCH_PRODUCTS_SUCCESS:
+      case FETCH_PRODUCT_SUCCESS:
       return action.count || 0;
       default:
       return state
@@ -71,7 +85,22 @@ export default combineReducers({
 })
 
 //Selectors
-export const getProduct = (state, id) => state.products.byId[id];
+//old
+export const getProductsByCollection = (state, slug) => {
+    const collection = getCollectionBySlug(state, slug);
+    if(collection) {
+        return collection.products.map(product => product.id);
+    }
+}
+
+//new
+export const getProduct = (state, id) => state.products.byId[id] || null;
+
+export const getProducts = (state) => state.products.allIds;
+
+export const getProductCount = (state) => state.products.total;
+
+export const getAllProducts = (state) => state.products.allIds.map(id => getProduct(state, id));
 
 export const getProductFeature = (state, id) => {
   const product = getProduct(state, id);
@@ -83,15 +112,6 @@ export const getProductFeature = (state, id) => {
   }
 }
 
-export const getProducts = (state) => state.products.allIds;
-
-export const getProductsByCollection = (state, slug) => {
-    const collection = getCollectionBySlug(state, slug);
-    if(collection) {
-        return collection.products.map(product => product.id);
-    }
-}
-
 export const getProductIDBySlug = (state, slug) => state.products.allIds.find(id => state.products.byId[id].slug === slug);
 
 export const getProductBySlug = (state, slug) => {
@@ -99,4 +119,16 @@ export const getProductBySlug = (state, slug) => {
     if (productId) { return getProduct(state, productId) }
 }
 
-export const getAllProducts = (state) => state.products.allIds.map(id => getProduct(state, id));
+export const getProductsFromTypes = (state, types) => {
+  const products = state.products.allIds;
+  if (types.length < 1) {
+    return products;
+  }
+  return products.filter(id => {
+      const product = getProduct(state, id);
+      if (product && product.type) {
+        return types.indexOf(product.type) !== -1;
+      }
+      return false;
+  });
+};
