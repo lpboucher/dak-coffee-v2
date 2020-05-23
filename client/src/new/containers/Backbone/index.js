ef8d3180e7c8d5523ce6765b7a607c9d6b64ee4a
LP Boucher (AMS) <louis.boucher@achtzig20.de>
2020-05-24 00:38:18 +0200

    simplified data fetching, only in relevant component

--- client/src/new/containers/Backbone/index.js
+++ client/src/new/containers/Backbone/index.js
@@ -1,7 +1,5 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getProducts, fetchProducts } from '../../../ducks/products';
-import { getSubscriptions, fetchSubscriptions } from '../../../ducks/subscriptions';
+import { useDispatch } from 'react-redux';
 import { openPromotion } from '../../../ducks/views';
 import { initializeCart } from '../../../ducks/cart';
 import { schemaBuilder } from '../../../components/utils/SEO/schema';
@@ -14,22 +12,11 @@ const BackBoneContainer = ({children}) => {
   const schema = schemaBuilder('WebSite', 'https://dakcoffeeroasters.com');
   const dispatch = useDispatch();
 
-  const products = useSelector(state => getProducts(state));
-  const plans = useSelector(state => getSubscriptions(state));
-
   useEffect(() => {
     dispatch(initializeCart());
     //toast.success("Wow so easy !");
     setTimeout(() => openPromotion(), 5000);
   }, [dispatch]);
-
-  useEffect(() => {
-    if (products && products.length < 1) dispatch(fetchProducts())
-  }, [dispatch, products]);
-
-  useEffect(() => {
-    if (plans && plans.length < 1) dispatch(fetchSubscriptions())
-  }, [dispatch, plans]);
 
   useEffect(() => {
     return () => {
--- client/src/new/containers/Products/ProductsListing/index.js
+++ client/src/new/containers/Products/ProductsListing/index.js
@@ -1,22 +1,20 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchCollections, getCollections } from '../../../../ducks/collections';
-import { getProductsByCollection } from '../../../../ducks/products';
+import { fetchProducts, getProductsFromTypes } from '../../../../ducks/products';
 
 import ProductsListing from '../../../components/Products/ProductsListing';
 import SkeletonCardList from '../../../components/Share/Skeletons/SkeletonCardList';
 
-const ProductsListingContainer = ({collection}) => {
-    const collections = useSelector(state => getCollections(state));
-    const productIds = useSelector(state => getProductsByCollection(state, collection));
+const ProductsListingContainer = ({productTypes = []}) => {
+    const productIds = useSelector(state => getProductsFromTypes(state, productTypes));
 
     const dispatch = useDispatch();
 
     useEffect(() => {
-        if (collections && collections.length < 1) {
-            dispatch(fetchCollections());
+        if (productIds && productIds.length < 1) {
+            dispatch(fetchProducts());
         }
-    }, [collections, dispatch]);
+    }, []);
 
     return (
       <>
