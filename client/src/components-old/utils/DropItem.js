44639124dce6e524cb4f14abf4884931b1e8b2b3
LP Boucher (AMS) <louis.boucher@achtzig20.de>
2020-10-24 15:20:02 +0200

    remove old files

--- client/src/components-old/utils/DropItem.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-import { NavLink } from 'react-router-dom';
-
-const DropItem = styled(NavLink)`
-   text-transform: uppercase;
-   padding: 10px 0;
-`
-
-export default DropItem
--- client/src/components-old/utils/HeadingWithLB.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-import { Heading } from 'grommet';
-
-const HeadingWithLB = styled(Heading)`
-    white-space: pre;
-`
-
-export default HeadingWithLB
