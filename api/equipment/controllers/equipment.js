5d8f2a6fd22b6628b0914813e9e5b6469c0b8e19
LP Boucher (AMS) <louis.boucher@achtzig20.de>
2020-10-25 11:35:55 +0100

    equipment

--- api/equipment/controllers/equipment.js
+++ api/equipment/controllers/equipment.js
@@ -5,4 +5,26 @@
  * to customize this controller
  */
 
-module.exports = {};
+const baseFields = {
+  slug : 1,
+  name : 1,
+  description : 1,
+  short : 1,
+  price : 1,
+  images : 1,
+  type: 1,
+  id: 1,
+  isActive: 1
+};
+
+const getEquipments = async (ctx) => {
+  const includedFields = {
+    ...baseFields,
+  };
+  const equipments = await strapi.query('equipment').model.find(ctx.query, includedFields);
+  ctx.send({equipment: equipments});
+};
+
+module.exports = {
+  getEquipments,
+};
