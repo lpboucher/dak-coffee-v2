59c7874e9cdc8a7ecf27887291ca00ef40691b5b
LP Boucher (AMS) <louis.boucher@achtzig20.de>
2020-10-25 11:52:20 +0100

    remove logs

--- api/utils/dataFormats.js
+++ api/utils/dataFormats.js
@@ -3,16 +3,11 @@
 // takes result of model query and combines multiple arrays into single array of ids
 const combineArray = (collection, key) => {
   const collectionObject = collection.toObject();
-  console.log('collectionObject', collectionObject);
   let combinedArray = [];
 
   Object.keys(collectionObject).map(property => {
-    console.log('key', property);
     if (Array.isArray(collectionObject[property])) {
-      console.log('is array', collectionObject[property]);
-      console.log('to combine', ...collectionObject[property].map(p => p.id));
       combinedArray = [...new Set([...combinedArray, ...collectionObject[property].map(p => p.id)])];
-      console.log('combined', combinedArray);
       delete collectionObject[property];
     }
   });
