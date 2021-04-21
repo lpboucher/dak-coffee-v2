e2adb81cd842c4c870a615ca4e165f57a399fe00
LP Boucher (AMS) <louis.boucher@achtzig20.de>
2021-04-21 16:01:32 +0200

    update base config and setup for v3

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import Backbone from './components/Backbone';
 import './App.css';
+import './Snipcart.css';
 
 const HomePage = lazy(() => import('./pages/Home'));
 const ShopPage = lazy(() => import('./pages/Shop'));
