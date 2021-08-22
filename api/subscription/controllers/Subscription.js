5abbd525fb595659d840f4b0d725dc855dfadd11
LP Boucher (AMS) <louis.boucher@achtzig20.de>
2021-08-22 16:44:28 +0200

    are routes for managing subscriptions

--- api/subscription/controllers/Subscription.js
+++ api/subscription/controllers/Subscription.js
@@ -1,22 +1,78 @@
 'use strict';
+const snipcart = require('snipcart-api');
 
 const getSubscriptions = async (ctx) => {
-  const includedFields = {
-    slug : 1,
-    name : 1,
-    description : 1,
-    short : 1,
-    price : 1,
-    images : 1,
-    type: 1,
-    id: 1,
-    isActive: 1
-  };
-  const subscriptions = await strapi.query('subscription').model.find({}, includedFields);
-  ctx.send(subscriptions);
+    const includedFields = {
+        slug : 1,
+        name : 1,
+        description : 1,
+        short : 1,
+        price : 1,
+        images : 1,
+        type: 1,
+        id: 1,
+        isActive: 1
+    };
+    const subscriptions = await strapi.query('subscription').model.find({}, includedFields);
+    ctx.send(subscriptions);
+};
+
+const snipcartDelete = async (ctx) => {
+    snipcart.configure('SECRET_API_KEY', strapi.config.currentEnvironment.snipcart);
+    const { id } = ctx.params;
+
+    try {
+        await snipcart.api.subscriptions.cancel({
+            urlParams: { id: id }
+        });
+
+        ctx.send({isSuccessful: true});
+        ctx.response.status = 204;
+    } catch(err) {
+        ctx.send({isSuccessful: false});
+        ctx.response.status = 400;
+    }
+};
+
+const snipcartPause = async (ctx) => {
+    snipcart.configure('SECRET_API_KEY', strapi.config.currentEnvironment.snipcart);
+    const { id } = ctx.params;
+
+    try {
+        await snipcart.api.subscriptions.pause({
+            urlParams: { id: id }
+        });
+
+        ctx.send({isSuccessful: true});
+        ctx.response.status = 200;
+    } catch(err) {
+        //console.log(err);
+        ctx.send({isSuccessful: false});
+        ctx.response.status = 400;
+    }
+};
+
+const snipcartResume = async (ctx) => {
+    snipcart.configure('SECRET_API_KEY', strapi.config.currentEnvironment.snipcart);
+    const { id } = ctx.params;
+
+    try {
+        await snipcart.api.subscriptions.resume({
+            urlParams: { id: id }
+        });
+
+        ctx.send({isSuccessful: true});
+        ctx.response.status = 200;
+    } catch(err) {
+        ctx.send({isSuccessful: false});
+        ctx.response.status = 400;
+    }
 };
 
 module.exports = {
-  getSubscriptions,
+    getSubscriptions,
+    snipcartDelete,
+    snipcartPause,
+    snipcartResume,
 };
 
