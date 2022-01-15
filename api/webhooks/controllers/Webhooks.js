4678ea6fa389648b54bffd02ca74ae071b0c0f32
LP Boucher (AMS) <louis.boucher@achtzig20.de>
2022-01-15 16:57:06 +0100

    change(webhooks): add tax webhook

--- api/webhooks/controllers/Webhooks.js
+++ api/webhooks/controllers/Webhooks.js
@@ -1,130 +1,158 @@
 'use strict';
 const {
-  hasNoPhysical,
-  hasFreeOption,
-  hasPickUpOption,
-  hasColdBrew,
-  getFreeShippingOptions,
-  getShippingRateOptions,
-  hasGiftCard,
-  isGiftCard,
-  isFromRegion,
-  createShippingParcel
+    hasNoPhysical,
+    hasFreeOption,
+    hasPickUpOption,
+    hasColdBrew,
+    getFreeShippingOptions,
+    getShippingRateOptions,
+    hasGiftCard,
+    isGiftCard,
+    isFromRegion,
+    isFromNL,
+    createShippingParcel
 } = require('../utils/shipping/tools');
 const promo = require('../../promo/controllers/promo');
 const groupBy = require('../../utils/dataFormats').groupBy;
 
 const getTaxes = (ctx) => {
-  const orderData = ctx.request.body.content;
-  let taxes = [];
-  if(!isTaxCollected(orderData.billingAddress.country)) {
-    taxes.push({'name': 'No Tax', 'amount': 0, 'rate': 0, 'numberForInvoice': 'TAX-000', 'includedInPrice': true});
-  } else {
-    taxes = [...taxes, ...aggregateItemTaxes(orderData.items.map(item => getTaxPerItem(item)))];
-  }
-  return {'taxes': taxes};
+    const orderData = ctx.request.body.content;
+    let taxes = [];
+    if(!isTaxCollected(orderData.billingAddress.country)) {
+        taxes.push({'name': 'No Tax', 'amount': 0, 'rate': 0, 'numberForInvoice': 'TAX-000', 'includedInPrice': true});
+    } else {
+        taxes = [...taxes, ...aggregateItemTaxes(orderData.items.map(item => getTaxPerItem(item)))];
+    }
+    return {'taxes': taxes};
 };
 
 const getShippingRates = (ctx) => {
-  const orderData = ctx.request.body.content;
-  const summary = { currency: orderData.currency, total: orderData.itemsTotal, shipTo: orderData.shippingAddress.country};
-  let rates = [];
-  if (hasNoPhysical(orderData.items)) {
-    return {'rates': [{'cost': 0, 'description': 'Digital Product'}]};
-  }
+    const orderData = ctx.request.body.content;
+    const summary = { currency: orderData.currency, total: orderData.itemsTotal, shipTo: orderData.shippingAddress.country};
+    let rates = [];
+    if (hasNoPhysical(orderData.items)) {
+        return {'rates': [{'cost': 0, 'description': 'Digital Product'}]};
+    }
 
-  if (hasFreeOption(orderData.items, summary, hasColdBrew(orderData.items))) {
-    rates = [...rates, ...getFreeShippingOptions(summary.shipTo)];
-  }
+    if (hasFreeOption(orderData.items, summary, hasColdBrew(orderData.items))) {
+        rates = [...rates, ...getFreeShippingOptions(summary.shipTo)];
+    }
 
-  if (hasPickUpOption(summary.shipTo)) {
-    rates = [...rates, {'cost': 0, 'description': 'Pickup at Nieuwendammerdijk 526M-8. Mon/Wed afternoons, 48h after order'}, {'cost': 3.5, 'description': 'Bike delivery in AMS, MON/WED'}];
-  }
+    if (hasPickUpOption(summary.shipTo)) {
+        rates = [...rates, {'cost': 0, 'description': 'Pickup at Nieuwendammerdijk 526M-8. Mon/Wed afternoons, 48h after order'}, {'cost': 3.5, 'description': 'Bike delivery in AMS, MON/WED'}];
+    }
 
-  rates = [...rates, ...getShippingRateOptions(summary.currency, summary.shipTo, hasColdBrew(orderData.items))];
-  return {'rates': rates};
+    rates = [...rates, ...getShippingRateOptions(summary.currency, summary.shipTo, hasColdBrew(orderData.items))];
+    return {'rates': rates};
 };
 
 const handleEvent = async (ctx) => {
-  const event = ctx.request.body;
-  const { items, user, shippingAddress, invoiceNumber, email } = event.content;
+    const event = ctx.request.body;
+    const { items, user, shippingAddress, invoiceNumber, email } = event.content;
 
-  switch (event.eventName) {
+    switch (event.eventName) {
 
-    case 'order.completed':
-      if (hasGiftCard(items)) {
-        const giftCard = items.find(item => isGiftCard(item));
-        try {
-          const promoCode = await promo.generateSnipcartPromo('gift', giftCard.unitPrice);
-          await promo.sendCodeEmail(user, promoCode);
-        } catch(err) {
-          if (Array.isArray(err)) {
-            err.map(oneerror => console.log(oneerror.messages));
-          } else {
-            console.log(err);
-          }
-        }
-      }
-      if (isFromRegion('EU', shippingAddress.country)) {
-        try {
-          await createShippingParcel(shippingAddress, email, invoiceNumber);
-        } catch(err) {
-          if (Array.isArray(err)) {
-            err.map(oneerror => console.log(oneerror.messages));
-          } else {
-            console.log(err);
-          }
-        }
-      }
-      ctx.response.status = 200;
-      ctx.send({});
+        case 'order.completed':
+            if (hasGiftCard(items)) {
+                const giftCard = items.find(item => isGiftCard(item));
+                try {
+                    const promoCode = await promo.generateSnipcartPromo('gift', giftCard.unitPrice);
+                    await promo.sendCodeEmail(user, promoCode);
+                } catch(err) {
+                    if (Array.isArray(err)) {
+                        err.map(oneerror => console.log(oneerror.messages));
+                    } else {
+                        console.log(err);
+                    }
+                }
+            }
+            if (isFromRegion('EU', shippingAddress.country)) {
+                try {
+                    await createShippingParcel(shippingAddress, email, invoiceNumber);
+                } catch(err) {
+                    if (Array.isArray(err)) {
+                        err.map(oneerror => console.log(oneerror.messages));
+                    } else {
+                        console.log(err);
+                    }
+                }
+            }
+            ctx.response.status = 200;
+            ctx.send({});
 
-    default:
-      ctx.response.status = 200;
-      ctx.send({});
-  }
+        // eslint-disable-next-line no-fallthrough
+        default:
+            ctx.response.status = 200;
+            ctx.send({});
+    }
 };
 
 module.exports = {
-  getShippingRates,
-  getTaxes,
-  handleEvent,
+    getShippingRates,
+    getTaxes,
+    getWholesaleTaxes,
+    handleEvent,
+};
+
+const getWholesaleTaxes = async (ctx) => {
+    const orderData = ctx.request.body.content;
+    let taxes = [];
+    let orderingCustomer = {};
+
+    try {
+        orderingCustomer = await strapi.query('customer').model.findOne({ email:orderData.email });
+
+        console.log('customer', orderingCustomer);
+    } catch (err) {
+        orderingCustomer.VAT = null;
+    }
+
+    if(shouldVATBeCharged(orderData.billingAddress.country, orderingCustomer.VAT)) {
+        taxes = [...taxes, ...aggregateItemTaxes(orderData.items.map(item => getTaxPerItem(item)), {'includedInPrice': false, 'appliesOnShipping': true})];
+    } else {
+        taxes.push({'name': 'No Tax', 'amount': 0, 'rate': 0, 'numberForInvoice': 'TAX-000', 'includedInPrice': true});
+    }
+
+    return {'taxes': taxes};
 };
 
 // TO DO move to own util file
-const aggregateItemTaxes = (taxPerItem) => {
-  let taxes = [];
-  const baseTax = {'includedInPrice': true, 'appliesOnShipping': false};
-  const coffeeTaxTotal = getTaxRateTotal(taxPerItem, 0.09);
-  const otherTaxTotal = getTaxRateTotal(taxPerItem, 0.21);
-  if (coffeeTaxTotal > 0) {
-    taxes.push({'name': '9% VAT (incl.)', 'amount': coffeeTaxTotal, 'rate': 0.09, 'numberForInvoice': 'TAX-COF', ...baseTax})
-  }
-  if (otherTaxTotal > 0) {
-    taxes.push({'name': '21% VAT (incl.)', 'amount': otherTaxTotal, 'rate': 0.21, 'numberForInvoice': 'TAX-PROD', ...baseTax})
-  }
+const aggregateItemTaxes = (taxPerItem, taxOptions = {'includedInPrice': true, 'appliesOnShipping': false}) => {
+    let taxes = [];
+    const coffeeTaxTotal = getTaxRateTotal(taxPerItem, 0.09);
+    const otherTaxTotal = getTaxRateTotal(taxPerItem, 0.21);
+    if (coffeeTaxTotal > 0) {
+        taxes.push({'name': '9% VAT (incl.)', 'amount': coffeeTaxTotal, 'rate': 0.09, 'numberForInvoice': 'TAX-COF', ...taxOptions});
+    }
+    if (otherTaxTotal > 0) {
+        taxes.push({'name': '21% VAT (incl.)', 'amount': otherTaxTotal, 'rate': 0.21, 'numberForInvoice': 'TAX-PROD', ...taxOptions});
+    }
 
-  return taxes;
+    return taxes;
 };
 
 const getTaxPerItem = ({customFields, totalPrice}) => {
-  const taxRate = isCoffeeProduct(customFields) ? 0.09 : 0.21;
-  return {'amount': Math.round(taxRate * totalPrice * 100) / 100, 'rate': taxRate};
+    const taxRate = isCoffeeProduct(customFields) ? 0.09 : 0.21;
+    return {'amount': Math.round(taxRate * totalPrice * 100) / 100, 'rate': taxRate};
 };
 
 const getTaxRateTotal = (items, rate) => {
-  return items.reduce((total, one) => {
-    if (one.rate === rate) {
-      total += one.amount;
-    }
-    return total;
-  }, 0);
-}
+    return items.reduce((total, one) => {
+        if (one.rate === rate) {
+            total += one.amount;
+        }
+        return total;
+    }, 0);
+};
 
 const isTaxCollected = (country) => {
-  return isFromRegion('EU', country);
+    return isFromRegion('EU', country);
+};
+
+const shouldVATBeCharged = (country, customerVAT) => {
+    return isFromNL(country) && (isFromRegion('EU', country) && customerVAT != null);
 };
 
 const isCoffeeProduct = (itemFields) => {
-  return itemFields.some(field => field.name === 'Weight');
+    return itemFields.some(field => field.name === 'Weight');
 };
--- api/webhooks/utils/shipping/tools.js
+++ api/webhooks/utils/shipping/tools.js
@@ -3,132 +3,137 @@ const axios = require('axios');
 const shipConstants = require('./constants');
 
 const hasFreeOption = (items, orderSummary, withColdItem = false) => {
-  const { currency, total, shipTo } = orderSummary;
-  return (orderHasSubscriptions(items) && isFromRegion("EU", shipTo)) ||
+    const { currency, total, shipTo } = orderSummary;
+    return (orderHasSubscriptions(items) && isFromRegion('EU', shipTo)) ||
         (total > getThreshold(currency, shipTo, withColdItem));
 };
 
 const hasPickUpOption = (country) => {
-    return country.toLowerCase() === "NL".toLowerCase();
-}
+    return isFromNL(country);
+};
 
 const hasNoPhysical = (items) => {
-  return hasGiftCard(items, 'all');
+    return hasGiftCard(items, 'all');
 };
 
 const orderHasSubscriptions = (items) => {
-  return items.some(item =>
-    item.name === 'The Classics' ||
+    return items.some(item =>
+        item.name === 'The Classics' ||
     item.name === 'The Roaster\'s Choice' ||
     item.name === 'Dak Subscription' ||
     item.name === 'Dak Monthly Subscription'
-  );
+    );
 };
 
 const hasGiftCard = (items, callbackType='one') => {
-  const callback = {
-    one: items.some(item => isGiftCard(item)),
-    all: items.every(item => isGiftCard(item))
-  };
-  return callback[callbackType];
+    const callback = {
+        one: items.some(item => isGiftCard(item)),
+        all: items.every(item => isGiftCard(item))
+    };
+    return callback[callbackType];
 };
 
 const hasColdBrew = (items, callbackType='one') => {
-  const callback = {
-    one: items.some(item => isColdBrew(item)),
-    all: items.every(item => isColdBrew(item))
-  };
-  return callback[callbackType];
+    const callback = {
+        one: items.some(item => isColdBrew(item)),
+        all: items.every(item => isColdBrew(item))
+    };
+    return callback[callbackType];
 };
 
 const isGiftCard = (item) => {
-  return item.id === '5fc9102b62130e5379f0042e';
+    return item.id === '5fc9102b62130e5379f0042e';
 };
 
 const isColdBrew = (item) => {
-  return item.id === '608ebd5c8e9a182d5e36b8d9';
-  // return item.id === '608ebe6970f72e24357e87c7' dev id for cold brew;
+    return item.id === '608ebd5c8e9a182d5e36b8d9';
+    // return item.id === '608ebe6970f72e24357e87c7' dev id for cold brew;
 };
 
 const getThreshold = (currency, country, requiresCold = false) => {
-  /*const shippingThresholds = {
+    /*const shippingThresholds = {
     EUR: { NL: 30, EU: 50, NA: 50, World: 70 },
     CAD: { NL: 45, EU: 75, NA: 75, World: 100 },
   };*/
-  const thresholds = requiresCold ? shipConstants.SHIPPING_THRESHOLDS_BY_REGION_COLD : shipConstants.SHIPPING_THRESHOLDS_BY_REGION;
-  const locationCode = fromCountryToRegion(country);
-  return thresholds[currency.toUpperCase()][locationCode];
+    const thresholds = requiresCold ? shipConstants.SHIPPING_THRESHOLDS_BY_REGION_COLD : shipConstants.SHIPPING_THRESHOLDS_BY_REGION;
+    const locationCode = fromCountryToRegion(country);
+    return thresholds[currency.toUpperCase()][locationCode];
 };
 
 const isFromRegion = (region, country) => {
-  return shipConstants.COUNTRY_CODES_BY_REGION[region].indexOf(country) !== -1;
+    return shipConstants.COUNTRY_CODES_BY_REGION[region].indexOf(country) !== -1;
+};
+
+const isFromNL = (country) => {
+    return country.toLowerCase() === 'NL'.toLowerCase();
 };
 
 const getFreeShippingOptions = (country) => {
-  return isFromRegion('EU', country) ?
-    shipConstants.FREE_SHIPPING_BY_REGION['EU'] :
-    shipConstants.FREE_SHIPPING_BY_REGION['World'];
+    return isFromRegion('EU', country) ?
+        shipConstants.FREE_SHIPPING_BY_REGION['EU'] :
+        shipConstants.FREE_SHIPPING_BY_REGION['World'];
 };
 
 const getShippingZone = (country) => {
-  const zone = Object.keys(shipConstants.SHIPPING_ZONES).find(zone => shipConstants.SHIPPING_ZONES[zone].indexOf(country) !== -1);
-  return zone ? zone : '4';
+    const zone = Object.keys(shipConstants.SHIPPING_ZONES).find(zone => shipConstants.SHIPPING_ZONES[zone].indexOf(country) !== -1);
+    return zone ? zone : '4';
 };
 
 const getShippingRateOptions = (currency, country, withColdAddition=false) => {
-  const allRates = withColdAddition ? shipConstants.SHIPPING_RATES_BY_REGION_COLD : shipConstants.SHIPPING_RATES_BY_REGION;
-  return [allRates[currency.toUpperCase()][getShippingZone(country)]];
+    const allRates = withColdAddition ? shipConstants.SHIPPING_RATES_BY_REGION_COLD : shipConstants.SHIPPING_RATES_BY_REGION;
+    return [allRates[currency.toUpperCase()][getShippingZone(country)]];
 };
 
 const fromCountryToRegion = (country) => {
-  let locationCode;
-  if (country === 'NL') {
-    locationCode = 'NL';
-  } else if (isFromRegion('EU', country)) {
-    locationCode = 'EU';
-  } else if (isFromRegion('NA', country)) {
-    locationCode = 'NA';
-  } else {
-    locationCode = 'World';
-  }
-  return locationCode;
-}
+    let locationCode;
+    if (country === 'NL') {
+        locationCode = 'NL';
+    } else if (isFromRegion('EU', country)) {
+        locationCode = 'EU';
+    } else if (isFromRegion('NA', country)) {
+        locationCode = 'NA';
+    } else {
+        locationCode = 'World';
+    }
+    return locationCode;
+};
 
 const createShippingParcel = async (shippingAddress, email, invoiceNumber) => {
-  return await axios.post(
-    `${shipConstants.SHIPCLOUD_ENDPOINT}/parcels`,
-    {
-      parcel: {
-        name: shippingAddress.fullName,
-        address: shippingAddress.address1,
-        address_2: shippingAddress.address2,
-        city: shippingAddress.city,
-        postal_code: shippingAddress.postalCode,
-        request_label: false,
-        email: email,
-        country: shippingAddress.country,
-        order_number: invoiceNumber,
-      }
-    },
-    {
-      auth: {
-        username: strapi.config.currentEnvironment.sendcloudKey,
-        password: strapi.config.currentEnvironment.sendcloudSecret,
-      }
-    }
-  );
+    return await axios.post(
+        `${shipConstants.SHIPCLOUD_ENDPOINT}/parcels`,
+        {
+            parcel: {
+                name: shippingAddress.fullName,
+                address: shippingAddress.address1,
+                address_2: shippingAddress.address2,
+                city: shippingAddress.city,
+                postal_code: shippingAddress.postalCode,
+                request_label: false,
+                email: email,
+                country: shippingAddress.country,
+                order_number: invoiceNumber,
+            }
+        },
+        {
+            auth: {
+                username: strapi.config.currentEnvironment.sendcloudKey,
+                password: strapi.config.currentEnvironment.sendcloudSecret,
+            }
+        }
+    );
 };
 
 module.exports = {
-  hasFreeOption,
-  hasPickUpOption,
-  hasNoPhysical,
-  getFreeShippingOptions,
-  getShippingRateOptions,
-  isFromRegion,
-  hasGiftCard,
-  hasColdBrew,
-  isGiftCard,
-  isColdBrew,
-  createShippingParcel
+    hasFreeOption,
+    hasPickUpOption,
+    hasNoPhysical,
+    getFreeShippingOptions,
+    getShippingRateOptions,
+    isFromRegion,
+    isFromNL,
+    hasGiftCard,
+    hasColdBrew,
+    isGiftCard,
+    isColdBrew,
+    createShippingParcel
 };
