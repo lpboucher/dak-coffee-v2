39448c7fe2d7aeededcd28168dfa59475367fc20
LP Boucher (AMS) <louis.boucher@achtzig20.de>
2022-12-31 15:22:58 +0100

    modify about page

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -54,6 +54,38 @@ const About = () => {
           />
         </Box>
       </TwoColLayout>
+      <TwoColLayout background="minor-3">
+        <Box height={layout[`twoColLayoutHeight_${mediaType}`] || twoColLayoutHeight}>
+          <CloudImage
+            img={`Intros/IMGL9756-bewerkt_j5avae`}
+            maxWidth={720}
+            fit='cover'
+          />
+        </Box>
+        <Box>
+          {t("about.showroom.title").split("<br>").map(show =>
+            <Heading margin={{bottom: "small"}}level={4}>{show}</Heading>
+          )}
+          {t("about.showroom.description").split("<br>").map(para =>
+            <Text margin={{top: "medium"}}>{para}</Text>
+          )}
+        </Box>
+      </TwoColLayout>
+      <TwoColLayout background="minor-4">
+        <Box>
+          <Heading color="mainWhite" level={4}>{t("about.team.title")}</Heading>
+          {t("about.team.description").split("<br>").map(para =>
+            <Text color="mainWhite" margin={{top: "medium"}}>{para}</Text>
+          )}
+        </Box>
+        <Box height={layout[`twoColLayoutHeight_${mediaType}`] || twoColLayoutHeight}>
+          <CloudImage
+            img={`Intros/IMGL9938-bewerkt_if4tlk`}
+            maxWidth={720}
+            fit='cover'
+          />
+        </Box>
+      </TwoColLayout>
     </>
   );
 };
