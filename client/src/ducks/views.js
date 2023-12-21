e810e664dcd4e327c7d255818a4d0641a3c1323d
lpboucher <lp_boucher@hotmail.com>
2023-12-21 14:12:19 +0100

    remove message pop-up, add inline message

--- client/src/ducks/views.js
+++ client/src/ducks/views.js
@@ -150,7 +150,7 @@ const initialState = {
 isMobileOpen: false,
 isProcessing: true,
 isModalOpen: false,
-isMessageOpen: true,
+isMessageOpen: false,
 //to be removed once fetching is specific to products
 isFetching: false,
 processingText: "loading.initial",
