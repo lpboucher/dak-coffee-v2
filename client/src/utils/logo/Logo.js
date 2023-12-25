5ac64822ee010e73c6122c414e86dc9a71098706
lpboucher <lp_boucher@hotmail.com>
2023-12-25 17:19:51 +0100

    add width height

--- client/src/utils/logo/Logo.js
+++ client/src/utils/logo/Logo.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Logo = ({height, width}) => {
     return (
-      <svg id="Layer_2" data-name="Layer 2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 364.95 51.15">
+      <svg id="Layer_2" data-name="Layer 2" xmlns="http://www.w3.org/2000/svg" width={width} height={height} viewBox="0 0 364.95 51.15">
       <g id="Laag_1" data-name="Laag 1">
         <g>
           <g>
